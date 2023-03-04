0320789ebc5fb275442a3278cdb02d74afc4fd8c
PHANGWilly <pro.phang.willy@gmail.com>
2023-03-04 23:37:34 +0100

    adding data/data.json planetes url

---
+++ src/js/maps.js
@@ -0,0 +1,21 @@
+let resultat
+const planetes = document.querySelector('.planetes');
+console.log(planetes);
+
+fetch("/data/data.json")
+.then((reponse) => {
+    return reponse.json();
+})
+.then((data) => {
+    resultat = data 
+    for (let i = 0; i < resultat["planetes"].length; i++) {
+        const card = `
+            <article class="planete" data-planete="${resultat["planetes"][i]["nom"]}">
+                <a href="#" title="Direction ${resultat["planetes"][i]["nom"]} !">
+                    <img src="${resultat["planetes"][i]["image"]}">
+                </a>
+            </article>
+        `;
+        planetes.insertAdjacentHTML('beforeend', card);
+    };
+})
